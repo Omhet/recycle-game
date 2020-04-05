f51c486dd21437e294691a36e86b7ea6d372d52a
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-05 16:18:32 +0300

    feat: add balls

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -11,15 +11,9 @@ export default class Main extends Scene {
   create() {
     const { width, height } = this.game.config;
 
-    const worldBounds = this.matter.world.setBounds(0, 0, width, height);
+    this.worldBounds = this.matter.world.setBounds(0, 0, width, height);
 
-    this.ball = new Ball({ scene: this, x: width / 2 });
-
-    this.matterCollision.addOnCollideStart({
-      objectA: this.ball,
-      objectB: [worldBounds.walls.left, worldBounds.walls.right],
-      callback: this.handleBallCollideWalls,
-    });
+    this.addBalls();
 
     const swipe = this.gestures.add.swipe({
       threshold: 1,
@@ -27,6 +21,20 @@ export default class Main extends Scene {
       direction: '8dir',
     });
     swipe.on('swipe', this.handleSwipe, this);
+  }
+
+  addBalls() {    
+    this.balls = new Array(2).fill(null).map(() => {
+      const ball = new Ball({ scene: this });
+      ball.setVisible(false);
+      return ball;
+    });
+
+    this.matterCollision.addOnCollideStart({
+      objectA: this.balls,
+      objectB: [this.worldBounds.walls.left, this.worldBounds.walls.right],
+      callback: this.handleBallCollideWalls,
+    });
   }
 
   handleBallCollideWalls() {
--- src/sprites/Ball.js
+++ src/sprites/Ball.js
@@ -1,14 +1,16 @@
-import { Physics } from 'phaser';
+import { Physics, Math } from 'phaser';
 import { OBJECTS } from '../constants';
 
 export default class Ball extends Physics.Matter.Sprite {
-  constructor({ scene, x }) {
-    super(scene.matter.world, x, scene.game.config.height, OBJECTS.BALL);
-    scene.add.existing(this);
+  constructor({ scene }) {
+    const { width, height } = scene.game.config;
     const size = 110;
+    const x = Math.Between(size, width - size);
+    const y = height - size;
+    super(scene.matter.world, x, y, OBJECTS.BALL);
+    scene.add.existing(this);
     this.setSize(size, size)
       .setDisplaySize(size, size)
-      .setBounce(0.95)
-      .setVelocityY(-20);
+      .setBounce(0.95);
   }
 }
