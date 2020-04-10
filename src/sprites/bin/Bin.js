01e013eb3f4585aba55c213c677dd74d5790f6e8
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-10 16:19:34 +0300

    feat: replace paper bin sprite with new one and update Bin coords

--- src/sprites/bin/Bin.js
+++ src/sprites/bin/Bin.js
@@ -3,16 +3,16 @@ import { Physics } from 'phaser';
 export default class Bin extends Physics.Matter.Sprite {
   constructor({ scene, x, key, type }) {
     const { height } = scene.game.config;
-    const size = 400;
 
-    super(scene.matter.world, x, height - size * 0.75);
+    super(scene.matter.world, x, height);
 
-    this.binImage = scene.add.image(x, height - size / 2, key);
-    this.binImage.setDisplaySize(size, size);
+    this.binImage = scene.add.image(x, height, key);
+    this.binImage.setScale(0.8).setOrigin(0.5, 1);
 
-    this.setDisplaySize(size * 0.68, 10)
+    this.setDisplaySize(this.binImage.width * 0.8, 10)
       .setSensor(true)
       .setStatic(true);
+    this.y = height - this.binImage.height * 0.8;
 
     this.type = type;
     this.fill = 0;
