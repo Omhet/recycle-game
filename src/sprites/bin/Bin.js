87b3f4179ecc36ce62cbda3fbdce9b79fba78974
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-10 19:18:58 +0300

    feat: add glass bin sprite and readjust bin coords

--- src/sprites/bin/Bin.js
+++ src/sprites/bin/Bin.js
@@ -3,16 +3,16 @@ import { Physics } from 'phaser';
 export default class Bin extends Physics.Matter.Sprite {
   constructor({ scene, x, key, type }) {
     const { height } = scene.game.config;
+    const size = 400;
 
-    super(scene.matter.world, x, height);
+    super(scene.matter.world, x, height - size);
 
     this.binImage = scene.add.image(x, height, key);
-    this.binImage.setScale(0.8).setOrigin(0.5, 1);
+    this.binImage.setDisplaySize(size * 0.92, size).setOrigin(0.5, 1);
 
-    this.setDisplaySize(this.binImage.width * 0.8, 10)
+    this.setDisplaySize(size * 0.95, 10)
       .setSensor(true)
       .setStatic(true);
-    this.y = height - this.binImage.height * 0.8;
 
     this.type = type;
     this.fill = 0;
