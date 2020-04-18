f5b49bcbf9ddfe32430735481575fe66ba85fcb1
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-18 19:16:43 +0300

    feat: tween icon on every right bin hit

--- src/texts/Score.js
+++ src/texts/Score.js
@@ -1,20 +1,16 @@
 import { GameObjects } from 'phaser';
 import { images } from '../constants';
 
-const scoreTemplate = score => `${score}`;
-
 export default class Score extends GameObjects.Text {
   constructor({ scene, text }) {
     const offset = 16;
-    const icon = scene.add
-      .image(offset, offset, images.icon)
-      .setOrigin(0, 0)
-      .setDepth(1);
+    const icon = scene.add.image(offset, offset, images.icon).setDepth(1);
+    icon.setPosition(offset + icon.width / 2, offset + icon.height / 2);
     super(
       scene,
       icon.width + offset * 2,
       icon.height / 2 - offset * 1.5,
-      scoreTemplate(text),
+      text,
       {
         fontSize: 80,
         fontStyle: 'bold',
@@ -32,6 +28,21 @@ export default class Score extends GameObjects.Text {
     scene.add.existing(this);
 
     this.icon = icon;
+
+    this.iconTween = scene.tweens.add({
+      targets: [this.icon],
+      duration: 400,
+      ease: 'Quad.easeOut',
+      props: {
+        angle: {
+          value: '+=360',
+        },
+        scale: {
+          value: '+=0.2',
+          yoyo: true,
+        },
+      },
+    });
   }
 
   dispose() {
@@ -40,6 +51,7 @@ export default class Score extends GameObjects.Text {
   }
 
   setScore = score => {
-    this.setText(scoreTemplate(score));
+    this.iconTween.restart();
+    this.setText(score);
   };
 }
