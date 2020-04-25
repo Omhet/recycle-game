7abc118414b6576489f68ffb7a2fbbfb42bfecae
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-25 21:54:02 +0300

    gitploy

---
+++ precache-manifest.692effdceaf15c7acf56850f3139ea0c.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c49eef0ffcc3fe41c96d741453c15a",
+    "url": "/recycle-game/index.html"
+  },
+  {
+    "revision": "662ac939706c43cb3339",
+    "url": "/recycle-game/static/css/main.fbff271e.chunk.css"
+  },
+  {
+    "revision": "a5d732f8d55934359166",
+    "url": "/recycle-game/static/js/2.9af8529f.chunk.js"
+  },
+  {
+    "revision": "5ff6ee79e1e13ccfbb7c97375aed19b5",
+    "url": "/recycle-game/static/js/2.9af8529f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "662ac939706c43cb3339",
+    "url": "/recycle-game/static/js/main.b572f1cd.chunk.js"
+  },
+  {
+    "revision": "d388128da09fafa55297",
+    "url": "/recycle-game/static/js/runtime-main.b5a16dc2.js"
+  },
+  {
+    "revision": "b4c6ce66a06aa4a091901f52899cfbbc",
+    "url": "/recycle-game/static/media/ball.b4c6ce66.png"
+  },
+  {
+    "revision": "bc74d347c9e9928fcd5dcd38a3ffe190",
+    "url": "/recycle-game/static/media/bottle.bc74d347.png"
+  },
+  {
+    "revision": "c1fe392098c6f9b6a064627399e1a905",
+    "url": "/recycle-game/static/media/bottle.c1fe3920.png"
+  },
+  {
+    "revision": "704b02d916c53e62670424a9cac012bb",
+    "url": "/recycle-game/static/media/can.704b02d9.png"
+  },
+  {
+    "revision": "41699a50c034b4a936474bb850a89fc6",
+    "url": "/recycle-game/static/media/city-back.41699a50.png"
+  },
+  {
+    "revision": "7a01eff4a05a4e61611efa47a5379eec",
+    "url": "/recycle-game/static/media/city.7a01eff4.png"
+  },
+  {
+    "revision": "c2b09be0fb305ea64bf66f2fb4c97466",
+    "url": "/recycle-game/static/media/cloud-0.c2b09be0.png"
+  },
+  {
+    "revision": "069eb259b9aff918d55406e710962786",
+    "url": "/recycle-game/static/media/cloud-1.069eb259.png"
+  },
+  {
+    "revision": "504be3ed64f74afdbf1ad7a67a14fa35",
+    "url": "/recycle-game/static/media/cloud-2.504be3ed.png"
+  },
+  {
+    "revision": "1ed5285cfa56627a1d59909af7e9c9c0",
+    "url": "/recycle-game/static/media/dead.1ed5285c.png"
+  },
+  {
+    "revision": "573cb6fa4373934a061e20275e644d5e",
+    "url": "/recycle-game/static/media/dead.573cb6fa.png"
+  },
+  {
+    "revision": "6b793ffce21045df6c6d2662f4affc83",
+    "url": "/recycle-game/static/media/dead.6b793ffc.png"
+  },
+  {
+    "revision": "dae8b1a63ad9831f457dfe9e79f680bf",
+    "url": "/recycle-game/static/media/dead.dae8b1a6.png"
+  },
+  {
+    "revision": "9a5d9b6214f5ef743c283bfd81ed6b0e",
+    "url": "/recycle-game/static/media/ding.9a5d9b62.mp3"
+  },
+  {
+    "revision": "37c105d5f876840d1de82c5abc4cd450",
+    "url": "/recycle-game/static/media/floor.37c105d5.png"
+  },
+  {
+    "revision": "4d6c771cedf72f7ca49e5313e9817ffe",
+    "url": "/recycle-game/static/media/font.4d6c771c.fnt"
+  },
+  {
+    "revision": "b8d1bfa95668c8715d3d0da52a942e53",
+    "url": "/recycle-game/static/media/font.b8d1bfa9.png"
+  },
+  {
+    "revision": "4d34993d1e49f9b379a49403b4a03cfc",
+    "url": "/recycle-game/static/media/general.4d34993d.png"
+  },
+  {
+    "revision": "56f649e6519e8926dc19f9d2e7f86069",
+    "url": "/recycle-game/static/media/gradient.56f649e6.png"
+  },
+  {
+    "revision": "3098b70655caeb24f75883b013818d63",
+    "url": "/recycle-game/static/media/grass.3098b706.png"
+  },
+  {
+    "revision": "092e501dc36fd5d0c194c90eedd0ec6e",
+    "url": "/recycle-game/static/media/heart.092e501d.png"
+  },
+  {
+    "revision": "fffd7041adce023343970d0e42e42e0a",
+    "url": "/recycle-game/static/media/heartbeat.fffd7041.mp3"
+  },
+  {
+    "revision": "671ffdcfff3e3f3fad70d8e6e98ba537",
+    "url": "/recycle-game/static/media/icon.671ffdcf.png"
+  },
+  {
+    "revision": "09fb80fe4e116ebafcf95cae23bc7961",
+    "url": "/recycle-game/static/media/idle.09fb80fe.png"
+  },
+  {
+    "revision": "2979a853a0b545c5f8b337455953db8f",
+    "url": "/recycle-game/static/media/idle.2979a853.png"
+  },
+  {
+    "revision": "3391f1e790748780647ce5a4201f9d4f",
+    "url": "/recycle-game/static/media/idle.3391f1e7.png"
+  },
+  {
+    "revision": "f0727a5914d2293a2f6e91aeec2665a4",
+    "url": "/recycle-game/static/media/idle.f0727a59.png"
+  },
+  {
+    "revision": "12b27694045cc02e203a89fc4a6d59c1",
+    "url": "/recycle-game/static/media/joy.12b27694.png"
+  },
+  {
+    "revision": "92846bbd7febd1d3014949758fceb138",
+    "url": "/recycle-game/static/media/joy.92846bbd.png"
+  },
+  {
+    "revision": "a23e8d2357c48e5d4dcee367a9744f3e",
+    "url": "/recycle-game/static/media/joy.a23e8d23.mp3"
+  },
+  {
+    "revision": "e4e15212c662efe9e6287be853f7bd70",
+    "url": "/recycle-game/static/media/joy.e4e15212.png"
+  },
+  {
+    "revision": "fa11b230aae1041f9f51631347bea970",
+    "url": "/recycle-game/static/media/joy.fa11b230.png"
+  },
+  {
+    "revision": "7c7e307cc10d219bb3d36a204f7a60e3",
+    "url": "/recycle-game/static/media/logo.7c7e307c.png"
+  },
+  {
+    "revision": "040c9d02ced31c04882e48eb9f66e97d",
+    "url": "/recycle-game/static/media/main.040c9d02.mp3"
+  },
+  {
+    "revision": "38c1f5de9bd60add475def02cfb1cf31",
+    "url": "/recycle-game/static/media/pavement.38c1f5de.png"
+  },
+  {
+    "revision": "468603cc9ca8be419268cfb2b27aece5",
+    "url": "/recycle-game/static/media/play.468603cc.png"
+  },
+  {
+    "revision": "0063f25d54d491848377ca4ee7124e45",
+    "url": "/recycle-game/static/media/pop.0063f25d.mp3"
+  },
+  {
+    "revision": "145c2dc555540cb485ef3fabb0248e5e",
+    "url": "/recycle-game/static/media/puke.145c2dc5.mp3"
+  },
+  {
+    "revision": "4b2f37db835c725e2c97000ef7c27adf",
+    "url": "/recycle-game/static/media/puke.4b2f37db.png"
+  },
+  {
+    "revision": "562258ce1cac9e6fdebda1b88ba7046a",
+    "url": "/recycle-game/static/media/puke.562258ce.png"
+  },
+  {
+    "revision": "a396652f15a04b11193ca1374ad6fd48",
+    "url": "/recycle-game/static/media/puke.a396652f.png"
+  },
+  {
+    "revision": "fb56354c244dac99cd4e5ca8e52a8707",
+    "url": "/recycle-game/static/media/puke.fb56354c.png"
+  },
+  {
+    "revision": "3a52957fe1635b6f871a4e972484a323",
+    "url": "/recycle-game/static/media/shrub.3a52957f.png"
+  },
+  {
+    "revision": "b3665476b1ac5fd55b8dd41149616712",
+    "url": "/recycle-game/static/media/stop.b3665476.mp3"
+  },
+  {
+    "revision": "408dd7fbd8523625291f7c6e4efca9d2",
+    "url": "/recycle-game/static/media/whoosh.408dd7fb.mp3"
+  }
+]);
